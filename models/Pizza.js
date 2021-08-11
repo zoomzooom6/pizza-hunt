0db278ed70b6b5e2feca9e9f94db633d32f113f7
zoomzoom6 <tonyhuang107@live.ca>
2021-08-11 16:22:07 -0400

    create pizza model, CRUD, and routes

---
+++ models/Pizza.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    toppings: []
+});
+
+//create Pizza model using Schema above
+const Pizza = model('Pizza', PizzaSchema);
+
+//export Pizza model
+module.export = Pizza;
